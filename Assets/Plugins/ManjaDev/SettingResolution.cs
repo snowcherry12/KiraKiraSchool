5c366ffe2559804547f14bb6c2a975cef2d2e648
Miftachul Huda <miftachul.huda.makruf@gmail.com>
2024-11-03 00:00:09 +0700

    Update Menu_GC

--- Assets/Plugins/ManjaDev/SettingResolution.cs
+++ Assets/Plugins/ManjaDev/SettingResolution.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Dropdown))]
-public class ResolutionDropdown : MonoBehaviour
+public class SettingResolution : MonoBehaviour
 {
     private List<Resolution> resolutions;
     void Start()
