20d609a91c9f1f3b15862aead10923cef415f602
Miftachul Huda <miftachul.huda.makruf@gmail.com>
2024-11-07 15:54:03 +0700

    Update Menu_GC

--- Assets/Plugins/GameCreator/Packages/Core/Runtime/Common/Console/Commands/CommandGraphic.cs
+++ Assets/Plugins/GameCreator/Packages/Core/Runtime/Common/Console/Commands/CommandGraphic.cs
@@ -15,6 +15,25 @@ namespace GameCreator.Runtime.Console
         public CommandGraphic() : base(new[]
         {
             new ActionOutput(
+                "fullscreen",
+                "Changes the fullscreen mode",
+                value =>
+                {
+                    switch (value)
+                    {
+                        case "True":
+                            Screen.fullScreen = true;
+                            break;
+                        case "False":
+                            Screen.fullScreen = false;
+                            break;
+                        default:
+                            return Output.Error($"Invalid value: {value}");
+                    }
+                    return Output.Success($"Graphic fullscreen = {Screen.fullScreen}");
+                }
+            ),
+            new ActionOutput(
                 "resolution",
                 "Changes the resolution",
                 value =>
@@ -27,19 +46,90 @@ namespace GameCreator.Runtime.Console
                 }
             ),
             new ActionOutput(
-                "fullscreen",
-                "Changes the fullscreen mode",
+                "quality",
+                "Changes the game quality",
                 value =>
                 {
-                    if (value == "true" || value == "false")
+                    switch (value)
                     {
-                        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, value == "true" ? true : false);
-                        return Output.Success($"Graphic fullscreen = {value}");
+                        case "0":
+                        case "1":
+                        case "2":
+                        case "3":
+                        case "4":
+                        case "5":
+                            QualitySettings.SetQualityLevel(Convert.ToInt32(value), true);
+                            break;
+                        default:
+                            return Output.Error($"Invalid value: {value}");
                     }
-                    else
-                    {
-                        return Output.Error($"Invalid value: {value}");
-                    }
+                    return Output.Success($"Graphic quality = {QualitySettings.names[QualitySettings.GetQualityLevel()]}");
+                }
+            ),
+            new ActionOutput(
+                "render-scale",
+                "Changes the render scale",
+                value =>
+                {
+                    float scale = Convert.ToSingle(value);
+                    QualitySettings.resolutionScalingFixedDPIFactor = scale;
+                    return Output.Success($"Graphic render scale = {QualitySettings.resolutionScalingFixedDPIFactor}");
+                }
+            ),
+            new ActionOutput(
+                "fsr",
+                "Changes the FSR scale",
+                value =>
+                {
+                    return Output.Success($"Graphic FSR = {value}");
+                }
+            ),
+            new ActionOutput(
+                "shadow",
+                "Changes the FSR scale",
+                value =>
+                {
+                    return Output.Success($"Graphic display scale = {value}");
+                }
+            ),
+            new ActionOutput(
+                "texture-quality",
+                "Changes the FSR scale",
+                value =>
+                {
+                    return Output.Success($"Graphic display scale = {value}");
+                }
+            ),
+            new ActionOutput(
+                "anisotropic-filtering",
+                "Changes the FSR scale",
+                value =>
+                {
+                    return Output.Success($"Graphic display scale = {value}");
+                }
+            ),
+            new ActionOutput(
+                "anti-aliasing",
+                "Changes the FSR scale",
+                value =>
+                {
+                    return Output.Success($"Graphic display scale = {value}");
+                }
+            ),
+            new ActionOutput(
+                "vsync",
+                "Changes the FSR scale",
+                value =>
+                {
+                    return Output.Success($"Graphic display scale = {value}");
+                }
+            ),
+            new ActionOutput(
+                "fps",
+                "Changes the FSR scale",
+                value =>
+                {
+                    return Output.Success($"Graphic display scale = {value}");
                 }
             ),
         }) { }
