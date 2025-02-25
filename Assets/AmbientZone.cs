f5f2a50c155d8a67cf06bdb4a1e3769f3f26aed3
MarufAwalian <marufawlion@gmail.com>
2025-02-25 14:50:57 +0700

    School Audio Ambient System
    - Outdoor Ambience
    - Indoor Ambience (with snapshots)
    - Rooftop Ambience
    - Canteen Ambience (with Snapshots)
    - Vending Machince
    - Front Door Slide

---
+++ Assets/AmbientZone.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+// ============== INSTRUCTION ==============
+// Create empty game object and add "Cinemachine Path" component
+// Add waypoints and draw shape -> set to "Looped" to close shape
+// Create another empty game object and add this script
+// Select "Path" as well as "Player" in the inspector
+// Add sound to the object
+
+namespace Cinemachine
+{
+    public class AmbientZone : MonoBehaviour
+    {
+        [Tooltip("Cinemachine Path to follow")]
+        public CinemachinePathBase m_Path;
+        [Tooltip("Character to track")]
+        public GameObject Player;
+
+        float m_Position;       // The position along the path to set the cart to in path units
+        private CinemachinePathBase.PositionUnits m_PositionUnits = CinemachinePathBase.PositionUnits.PathUnits;
+
+        void Update()
+        {
+            // Find closest point to the player along the path
+            SetCartPosition(m_Path.FindClosestPoint(Player.transform.position, 0, -1, 10));
+            // Define vectors for the dot product
+            Vector3 Sub = transform.position - Player.transform.position;
+            Vector3 Spline = transform.right;
+            // Attach object to player on enter
+            if (Vector3.Dot(Sub, Spline) < 0)
+            {
+                transform.position = Player.transform.position;
+                transform.rotation = Player.transform.rotation;
+            }
+        }
+
+        // Set cart's position to closest point
+        void SetCartPosition(float distanceAlongPath)
+        {
+            m_Position = m_Path.StandardizeUnit(distanceAlongPath, m_PositionUnits);
+            transform.position = m_Path.EvaluatePositionAtUnit(m_Position, m_PositionUnits);
+            transform.rotation = m_Path.EvaluateOrientationAtUnit(m_Position, m_PositionUnits);
+        }
+    }
+}
